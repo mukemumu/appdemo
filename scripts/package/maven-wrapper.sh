b09c10ab1e4e3ce284dc805f3479d27cd223e744
qisui <1355407724@qq.com>
2024-03-01 11:57:04 +0800

    修改maven-wrapper为buercorp账户

--- scripts/package/maven-wrapper.sh
+++ scripts/package/maven-wrapper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 替换下面的路径为你机器上对应的实际路径
-JAVA_HOME="/home/li-dongdong/apps/jdk"
-MAVEN_HOME="/home/li-dongdong/apps/maven"
+JAVA_HOME="/home/buercorp/apps/jdk"
+MAVEN_HOME="/home/buercorp/apps/maven"
 
 # 为Java和Maven执行指定使用的路径
 PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH
