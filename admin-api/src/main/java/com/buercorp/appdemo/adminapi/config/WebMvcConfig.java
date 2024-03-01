5dfc99c296b745e63223bd36d50f930b5ac4eb9c
tanghx <stdio1755228699@163.com>
2024-03-01 15:27:31 +0800

    跨域请求处理

--- admin-api/src/main/java/com/buercorp/appdemo/adminapi/config/WebMvcConfig.java
+++ admin-api/src/main/java/com/buercorp/appdemo/adminapi/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import com.buercorp.appdemo.adminapi.interceptor.AdminLoginInterceptor;
 import com.buercorp.appdemo.adminapi.interceptor.LocaleInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,5 +28,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeInterceptor);
         registry.addInterceptor(adminLoginInterceptor);
+    }
+
+    /**
+     * 跨域请求处理
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")         // 添加路径规则
+                .allowCredentials(true)               // 是否允许在跨域的情况下传递 Cookie
+                .allowedOriginPatterns("*")           // 允许请求来源的域规则
+                .allowedMethods("*")
+                .allowedHeaders("*");                // 允许所有的请求头
     }
 }
--- portal-api/src/main/java/com/buercorp/appdemo/portalapi/config/WebMvcConfig.java
+++ portal-api/src/main/java/com/buercorp/appdemo/portalapi/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import com.buercorp.appdemo.portalapi.interceptor.LocaleInterceptor;
 import com.buercorp.appdemo.portalapi.interceptor.UserLoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,5 +27,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeInterceptor);
         registry.addInterceptor(userLoginInterceptor);
+    }
+
+    /**
+     * 跨域请求处理
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")         // 添加路径规则
+                .allowCredentials(true)               // 是否允许在跨域的情况下传递 Cookie
+                .allowedOriginPatterns("*")           // 允许请求来源的域规则
+                .allowedMethods("*")
+                .allowedHeaders("*");                // 允许所有的请求头
     }
 }
